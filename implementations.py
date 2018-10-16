8813dff3e257930d5dd517f03dbbb5b0e5af9b6d
Sacha W <43576729+szwify@users.noreply.github.com>
2018-10-16 17:44:41 +0200

    Remoed print

--- implementations.py
+++ implementations.py
@@ -67,8 +67,7 @@ def least_squares_SGD(
             ws.append(w)
             losses.append(loss)
 
-        print("SGD({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+        
     return ws[-1], losses[-1]
 
 def ridge_regression(y, tx, lambda_):
